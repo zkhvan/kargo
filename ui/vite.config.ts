c47480ac20ee5d6efd7770add7978ecefae3c251
Rafal Pelczar <rafal@akuity.io>
2023-04-26 03:21:34 +0200

    feat(ui): Init react project

---
+++ ui/vite.config.ts
@@ -0,0 +1,19 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import tsConfigPaths from 'vite-tsconfig-paths';
+
+export const UI_VERSION = process.env.VERSION || 'development';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    outDir: 'build',
+    sourcemap: false
+  },
+  plugins: [tsConfigPaths(), react()],
+  server: {
+    port: 3333
+  }
+});
